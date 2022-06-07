6ac84d86056e945251ea596498a9abec7066bea6
Alex Ryan <alex.ryan@inspectorio.com>
2022-06-07 18:31:02 +0700

    Refactor and remove distracting docstrings

--- factory_pattern.py
+++ factory_pattern.py
@@ -2,92 +2,68 @@ from abc import ABC, abstractmethod
 
 
 class PaymentProcessor(ABC):
-    """Base class for Payment Processors."""
 
     @abstractmethod
     def make_payment(self, amount: float):
-        """Process a payment."""
         ...
 
 
 class StripePaymentProcessor(PaymentProcessor):
-    """Payment processor for making Stripe payments."""
 
     def make_payment(self, amount: float):
-        """Process a Stripe payment."""
-
-        # Do some Stripe specific payment stuff
         print(f"Made a Stripe payment of {amount}")
 
 
 class AdyenPaymentProcessor(PaymentProcessor):
-    """Payment processor for making Adyen payments."""
 
     def make_payment(self, amount: float):
-        """Process a Adyen payment."""
-
-        # Do some Adyen specific payment stuff
+        print("Did some Adyen specific stuff")
         print(f"Made an Adyen payment of {amount}")
 
 
 class PaymentService(ABC):
-    """An abstract service to handle the making of payments."""
 
     @abstractmethod
     def create_payment_processor(self):
-        """Factory method to create a concrete PaymentProcessor."""
         ...
 
     def make_payment(self, amount: float):
-        """Make a payment with the PaymentProcessor"""
-
+        self.verify_config()
         payment_processor = self.create_payment_processor()
         payment_processor.make_payment(amount)
     
     def verify_config(self):
-        """
-        Perform some business logic around verifying if the user has
-        payments setup.
-        """
-        ...
+        print("Verified payment config")
 
 
 class StripePaymentService(PaymentService):
-    """
-    A Stripe payment service that instantiates a StripePaymentProcessor
-    to handle payments.
-    """
 
     def create_payment_processor(self):
+        # You could do some specific Stripe setup stuff here
         return StripePaymentProcessor()
 
 
 class AdyenPaymentService(PaymentService):
-    """
-    An Adyen payment service that instantiates an AdyenPaymentProcessor 
-    to handle payments.
-    """
 
     def create_payment_processor(self):
         return AdyenPaymentProcessor()
 
 
 PAYMENT_SERVICE_MAP = {
-        "adyen": AdyenPaymentService,
-        "stripe": StripePaymentService,
-    }
+    "adyen": AdyenPaymentService,
+    "stripe": StripePaymentService,
+}
 
 
 def get_provider():
-    provider = None
+    provider_list = " or ".join(PAYMENT_SERVICE_MAP.keys())
     while True:
-        provider = input("Use Stripe or Adyen? ").lower()
+        provider = input(f"Use {provider_list}? ").lower()
         if provider in PAYMENT_SERVICE_MAP.keys():
             return provider
 
 
 def get_amount():
-    amount = None
     while True:
         amount = input("Enter transaction amount: ")
         try:
